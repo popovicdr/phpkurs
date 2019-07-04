c7214f97ce81b36cd7fb7152df7a45db95854ed1
VG011 <vg011@hotmail.com>
2019-07-04 21:09:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 echo 'Jos malo pa kraj'
 echo 'Ovo je tek kraj pocetka/ pocetak kraja <3 hsdggdjhdg';
-
+echo 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
 echo 'uskoro pocinjemo ozbiljno da radimo <3';
 
 echo 'test';
